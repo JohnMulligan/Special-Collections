45f0b46cc2d5a49df6453f65616afd8b72633703
Miles Olson <mpolson64@gmail.com>
2021-02-09 16:12:05 -0600

    Repelling bubble graph w bounding box

--- src/components/NetworkView.js
+++ src/components/NetworkView.js
@@ -1,39 +1,53 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Table, Button, Space } from "antd";
 import * as d3 from "d3";
 
 
 const NetworkView = (props) => {
+    const height = 200;
+    const width = 400;
+
     const d3Container = useRef(null);
 
     // Get dummy data
     var json = require("../POCdata.json")
-    // const data = JSON.parse(JSON.stringify(json));
-    const data = [12, 5, 6, 6, 9, 10];
-
+    const data = JSON.parse(JSON.stringify(json));
     console.log(data);
     
     useEffect(() => {
         const svg = d3.select(d3Container.current);
 
-    const h = 200;
 
-    const update = svg.selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => i * 70)
-      .attr("y", (d, i) => h - 10 * d)
-      .attr("width", 65)
-      .attr("height", (d, i) => d * 10)
-      .attr("fill", "green");
+        const simulation = d3.forceSimulation()
+            .nodes(data)
+            .force("charge_force", d3.forceManyBody())
+            .force("center_force", d3.forceCenter(width / 2, height / 2));
+
+        const radius = 5;
+        const node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("r", radius)
+            .attr("fill", "blue");
+        
+        const tickActions = () => {
+            node
+                .attr("cx", d => Math.max(radius, Math.min(width - radius, d.x)))
+                .attr("cy", d => Math.max(radius, Math.min(height - radius, d.y)));
+        };
+
+        simulation.on("tick", tickActions );
+
+
     }, [data]);
 
     return(
         <svg
             className="d3-component"
-            width={400}
-            height={200}
+            width={width}
+            height={height}
             ref={d3Container}
         />
     );
