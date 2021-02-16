03d35aeccf5186855d5f616227c42b71e0b5cebd
Charles <cva1@rice.edu>
2021-02-16 13:54:00 -0600

    Added dragging of nodes

--- src/components/NetworkView.js
+++ src/components/NetworkView.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 
 
+
 const NetworkView = (props) => {
-    const height = 600;
-    const width = 1000;
+    const height = 1000;
+    const width = 1500;
 
     const d3Container = useRef(null);
 
@@ -12,7 +13,7 @@ const NetworkView = (props) => {
     var data_json = require("../POCdata.json")
 
     const data = JSON.parse(JSON.stringify(data_json));
-    console.log(data);
+    // console.log(data);
     
     useEffect(() => {
         // create links
@@ -31,7 +32,8 @@ const NetworkView = (props) => {
             .nodes(data)
             .force("charge_force", d3.forceManyBody())
             .force("links", d3.forceLink(links).id( d => d["o:id"]))
-            .force("center_force", d3.forceCenter(width / 2, height / 2));
+            .force("center_force", d3.forceCenter(width / 2, height / 2 ))
+            .on("tick", tickActions);
 
         const g =  svg.append("g");
         const radius = 5;
@@ -42,9 +44,15 @@ const NetworkView = (props) => {
             .enter()
             .append("circle")
             .attr("r", radius)
-            .on("click", function (d, i) {
-                d3.select(this).style("fill", "#ffd1dc");
+            .on("dblclick", function (d, i) {
+                //d3.select(this).style("fill", "#ffd1dc");
+                console.log(d)
+                console.log(i)
             })
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended))
             .attr("fill", d => color(d["@type"][1]));
 
         const link = g
@@ -64,10 +72,21 @@ const NetworkView = (props) => {
               .attr('font-size', 12)
               .attr("font-family", "Nunito")
               .attr("fill", "#555")
-              .attr('dx', 10)
-              .attr('dy', 4);
+              .attr("x", node => node.x + 40)
+              .attr("y", node => node.y + 10)
+              .on("click", function(d, i) {
+                console.log(d);
+                console.log(i);
+            });
+            //   .attr('dx', 10)
+            //   .attr('dy', 4);
+            
+              
         
-        const tickActions = () => {
+        function tickActions() {
+            
+            console.log("ticked")
+
             node
                 .attr("cx", d => Math.max(radius, Math.min(width - radius, d.x)))
                 .attr("cy", d => Math.max(radius, Math.min(height - radius, d.y)));
@@ -79,15 +98,34 @@ const NetworkView = (props) => {
                 .attr("y2", function(d) { return d.target.y; });
 
             textElements
-                .attr("x", d => Math.max(radius, Math.min(width - radius, d.x)))
-                .attr("y", d => Math.max(radius, Math.min(height - radius, d.y)));
+                // .attr("x", d => Math.max(radius, Math.min(width - radius, d.x)))
+                // .attr("y", d => Math.max(radius, Math.min(height - radius, d.y)));
+                .attr("x", node => node["x"] + 10)
+                .attr("y", node => node["y"] + 4)
+                
         };
 
-        simulation.on("tick", tickActions );
+        function dragstarted() {
+            simulation.tick()
+            d3.select(this).raise();
+            d3.select(this).attr("stroke", "black")
+            g.attr("cursor", "grabbing");
+        }
+
+        function dragged(event, d) {
+            simulation.restart()
+            d3.select(this).attr("cx", d.x = event.x).attr("cy", d.y = event.y)
+        }
+
+        function dragended() {
+            d3.select(this).attr("stroke", null)
+            g.attr("cursor", null)
+        }
 
         svg.call(d3.zoom().extent([[0, 0], [2*width, 2*height]])
                 .scaleExtent([1, 8])
-                .on("zoom", (e) => g.attr("transform", e.transform)));
+                .on("zoom", (e) => g.attr("transform", e.transform)))
+
 
     }, [data]);
 
